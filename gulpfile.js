8238f6c9d176e6f60a05f7fe78a38f1a3a04900b
kenneth <wkkhchan@gmail.com>
2017-02-09 00:32:54 +0800

    setup gulp serve

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var livereload = require('gulp-livereload');
+var nodemon = require('nodemon');
 
 gulp.task('lint', function() {
     gulp.src('./js/*.js')
@@ -13,23 +15,35 @@ gulp.task('lint', function() {
 });
 
 gulp.task('sass', function() {
-    gulp.src('./scss/*.scss')
+    gulp.src('./scss/**/*.scss')
         .pipe(sass())
-        .pipe(gulp.dest('./public/css'));
+        .pipe(gulp.dest('./public/css'))
+        .pipe(livereload());
 });
 
 gulp.task('scripts', function() {
-    gulp.src('./js/*.js')
+    gulp.src('./js/**/*.js')
         .pipe(concat('all.js'))
         .pipe(gulp.dest('./dist'))
         .pipe(rename('all.min.js'))
         .pipe(uglify())
-        .pipe(gulp.dest('./public/js'));
+        .pipe(gulp.dest('./public/js'))
+        .pipe(livereload());
 });
 
 gulp.task('default',['lint', 'sass', 'scripts']);
 
-gulp.watch('js/**/*.js',['lint','scripts']);
-gulp.watch('scss/**/*.scss',['sass']);
+gulp.task('watch', function() {  
+    livereload.listen();
+    gulp.watch('scss/**/*.scss', ['sass']);
+    gulp.watch('js/**/.js', ['scripts']);
+});
 
+gulp.task('server',function(){
+  nodemon({
+    'script': 'index.js'
+  });
+});
+
+gulp.task('serve', ['server','watch']);
 
