354bdd381012a5d8e0114016a05c7a024e3066e9
kenneth <wkkhchan@gmail.com>
2017-02-08 23:07:59 +0800

    use gulp

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+require('gulp');
+var sass = require('node-sass');
 var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
+app.use('/js',express.static(__dirname + '/public/js'));
+app.use('/css',express.static(__dirname + '/public/css'));
+
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
@@ -12,6 +17,7 @@ app.set('view engine', 'ejs');
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
